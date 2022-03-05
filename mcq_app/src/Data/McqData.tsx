639ff834604767bc36e7c670bf7d00009eed08e8
GA-Himani <Himani.Bhatt@green-apex.com>
2022-03-05 15:37:01 +0530

    Engg AI Practice

---
+++ mcq_app/src/Data/McqData.tsx
@@ -0,0 +1,66 @@
+const McqData =[{
+lang:'english',
+questions:[
+{
+	id:0,
+	questionNo:1,
+	isAnswer:false,
+	answerValue:[],
+	question:"What is our National Bird",
+	answers:[
+"Hen","Peacock","Sparrow","Eagle"
+	],
+correctAnswer:'Peacock'
+},
+{
+	id:1,
+	questionNo:2,
+	isAnswer:false,
+	answerValue:[],
+	question:"React is developed by _______",
+	answers:[
+"facebook engineers","Google Engineers"
+	],
+correctAnswer:'facebook engineers'
+},
+{
+	id:2,
+	questionNo:3,
+	isAnswer:false,
+	answerValue:[],
+	question:"React is a MVC framework?",
+	answers:[
+"True","False"
+	],
+correctAnswer:'False'
+},
+{
+	id:3,
+	questionNo:4,
+	isAnswer:false,
+	answerValue:[],
+	question:"What is of the following matches",
+	columnOne:["tiger NationalBird","Peacock NationalFlag" , "Trianga NationalAnimal"],
+	answers:[
+"a-c,b-a,c-b",
+"a-b,c-a,b-c",
+"none of the above"
+	],
+correctAnswer:"a-c,b-a,c-b"
+},
+{
+	id:4,
+	questionNo:5,
+	isAnswer:false,
+	answerValue:[],
+	question:"what is frog",
+	answers:[
+"carnivorus","harvivorus","omnivorus","none of the above"
+	],
+correctAnswer:["carnivorus","harvivorus"]
+},
+]
+}]
+
+
+export default McqData
